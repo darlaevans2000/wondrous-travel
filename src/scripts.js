05f0a61e6884124e336909961d09718be00928bb
darlaevans2000 <darlaevans2000@gmail.com>
2021-08-08 15:38:18 -0600

    Add dayjs imports

--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,10 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
+import * as dayjs from 'dayjs'
+var isBetween = require('dayjs/plugin/isBetween')
+dayjs.extend(isBetween)
+
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
